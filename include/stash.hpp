51cfd867922b36301f49f6cdf3d69649c403a118
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-08-21 16:38:32 +0300

    Add incapsulation practices 🔐

--- include/stash.hpp
+++ include/stash.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
 
-struct Stash
+class Stash
 {
     int size;
     int quaintity;
     int next;
     unsigned char* storage;
 
+    void inflate(int increase);
+
+public:
     void initialize(int _size);
     void cleanup();
     int add(const void* element);
     void *fetch(int index);
     int count();
-    void inflate(int increase);
 };
