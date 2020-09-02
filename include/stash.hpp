6da690d9675ace73ebacd41f2a2233f463465a85
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2020-09-02 15:24:31 +0300

    Replace some functions with their inline analogs

--- include/stash.hpp
+++ include/stash.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include<iostream>
+#include<cassert>
 
 class Stash
 {
@@ -11,9 +13,30 @@ class Stash
     void inflate(int increase);
 
 public:
-    Stash(int _size, int init_size = 0);
-    ~Stash();
+    inline Stash(int _size, int init_size = 0) : size(_size), 
+        quaintity(0), next(0), storage(0)
+    {
+        inflate(init_size);
+    };
+
+    inline ~Stash()
+    {
+        if (storage != 0)
+        {
+            std::cout << "Freeing storage" << std::endl;
+            delete[] storage;
+        }
+    };
+
     int add(const void* element);
-    void *fetch(int index);
-    int count();
+    inline void* fetch(int index)
+    {
+        assert(0 <= index);
+        if (index >= next)
+            return 0;
+
+        return &(storage[index * size]);
+    };
+
+    inline int count() const { return next; };
 };
--- source/stash.cpp
+++ source/stash.cpp
@@ -5,15 +5,6 @@
 
 const int INCREMENT = 100;
 
-Stash::Stash(int _size, int init_size /* = 0 */)
-{
-    size = _size;
-    quaintity = 0;
-    storage = 0;
-    next = 0;
-    inflate(init_size);
-}
-
 int Stash::add(const void *element)
 {
     if (next >= quaintity)
@@ -32,23 +23,9 @@ int Stash::add(const void *element)
     return (next - 1);
 }
 
-void * Stash::fetch(int index)
-{
-    assert(0 <= index);
-    if(index >= next)
-        return 0;
-
-    return &(storage[index * size]);
-}
-
- int Stash::count()
- {
-     return next;
- }
-
 void Stash::inflate(int increase)
 {
-    assert(increase > 0);
+    assert(increase >= 0);
     int new_quantity = quaintity + increase;
     int new_bytes = new_quantity * size;
     int old_bytes = quaintity * size;
@@ -60,13 +37,4 @@ void Stash::inflate(int increase)
     delete[] storage;
     storage = buf;
     quaintity = new_quantity;
-}
-
-Stash::~Stash()
-{
-    if (storage != 0)
-    {
-        std::cout << "Freeing storage" << std::endl;
-        delete[] storage;
-    }
 }
